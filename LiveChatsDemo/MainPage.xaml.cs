4845c5610406089f52102ab6239948793390322e
Materal <cloomcmx1554@hotmail.com>
2025-07-15 15:39:19 +0800

    feat:添加示例程序

---
+++ LiveChatsDemo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace LiveChatsDemo;
+
+public sealed partial class MainPage : Page
+{
+    public TestViewModel ViewModel { get; } = new();
+    public MainPage() => InitializeComponent();
+}
+public class TestViewModel
+{
+    public ISeries[] Series { get; set; } =
+     [
+        new LineSeries<double>
+        {
+            Values = [2, 1, 3, 5, 3, 4, 6],
+            Fill = null
+        }
+     ];
+}
